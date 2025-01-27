0a1d002f26223fd4d375767c4a4006754b899202
mortifia <guillaume.casal.42@gmail.com>
2025-01-27 17:29:24 +0100

    mine

--- src/main.rs
+++ src/main.rs
@@ -1,164 +1,214 @@
-//! Shows how to iterate over combinations of query results.
+//! Demonstrates how to observe life-cycle triggers as well as define custom ones.
 
-use bevy::{color::palettes::css::ORANGE_RED, core_pipeline::bloom::Bloom, math::FloatPow, prelude::*};
+use bevy::{
+    prelude::*,
+    utils::{HashMap, HashSet},
+};
 use rand::{Rng, SeedableRng};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::BLACK))
-        .add_systems(Startup, generate_bodies)
-        .add_systems(FixedUpdate, (interact_bodies, integrate))
-        .add_systems(Update, look_at_star)
+        .init_resource::<SpatialIndex>()
+        .add_systems(Startup, setup)
+        .add_systems(Update, (draw_shapes, handle_click))
+        // Observers are systems that run when an event is "triggered". This observer runs whenever
+        // `ExplodeMines` is triggered.
+        .add_observer(
+            |trigger: Trigger<ExplodeMines>,
+             mines: Query<&Mine>,
+             index: Res<SpatialIndex>,
+             mut commands: Commands| {
+                // You can access the trigger data via the `Observer`
+                let event = trigger.event();
+                // Access resources
+                for e in index.get_nearby(event.pos) {
+                    // Run queries
+                    let mine = mines.get(e).unwrap();
+                    if mine.pos.distance(event.pos) < mine.size + event.radius {
+                        // And queue commands, including triggering additional events
+                        // Here we trigger the `Explode` event for entity `e`
+                        commands.trigger_targets(Explode, e);
+                    }
+                }
+            },
+        )
+        // This observer runs whenever the `Mine` component is added to an entity, and places it in a simple spatial index.
+        .add_observer(on_add_mine)
+        // This observer runs whenever the `Mine` component is removed from an entity (including despawning it)
+        // and removes it from the spatial index.
+        .add_observer(on_remove_mine)
         .run();
 }
 
-const GRAVITY_CONSTANT: f32 = 0.001;
-const NUM_BODIES: usize = 100;
-
-#[derive(Component, Default)]
-struct Mass(f32);
-#[derive(Component, Default)]
-struct Acceleration(Vec3);
-#[derive(Component, Default)]
-struct LastPos(Vec3);
 #[derive(Component)]
-struct Star;
-
-#[derive(Bundle, Default)]
-struct BodyBundle {
-    mesh: Mesh3d,
-    material: MeshMaterial3d<StandardMaterial>,
-    mass: Mass,
-    last_pos: LastPos,
-    acceleration: Acceleration,
+struct Mine {
+    pos: Vec2,
+    size: f32,
 }
 
-fn generate_bodies(
-    time: Res<Time<Fixed>>,
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let mesh = meshes.add(Sphere::new(1.0).mesh().ico(3).unwrap());
-
-    let color_range = 0.5..1.0;
-    let vel_range = -0.5..0.5;
-
-    // We're seeding the PRNG here to make this example deterministic for testing purposes.
-    // This isn't strictly required in practical use unless you need your app to be deterministic.
-    let mut rng = rand::rngs::StdRng::seed_from_u64(19878367467713);
-    for _ in 0..NUM_BODIES {
-        let radius: f32 = rng.gen_range(0.1..0.7);
-        let mass_value = FloatPow::cubed(radius) * 10.;
-
-        let position = Vec3::new(
-            rng.gen_range(-1.0..1.0),
-            rng.gen_range(-1.0..1.0),
-            rng.gen_range(-1.0..1.0),
-        )
-        .normalize()
-            * ops::cbrt(rng.gen_range(0.2f32..1.0))
-            * 15.;
-
-        commands.spawn((
-            BodyBundle {
-                mesh: Mesh3d(mesh.clone()),
-                material: MeshMaterial3d(materials.add(Color::srgb(
-                    rng.gen_range(color_range.clone()),
-                    rng.gen_range(color_range.clone()),
-                    rng.gen_range(color_range.clone()),
-                ))),
-                mass: Mass(mass_value),
-                acceleration: Acceleration(Vec3::ZERO),
-                last_pos: LastPos(
-                    position
-                        - Vec3::new(
-                            rng.gen_range(vel_range.clone()),
-                            rng.gen_range(vel_range.clone()),
-                            rng.gen_range(vel_range.clone()),
-                        ) * time.timestep().as_secs_f32(),
-                ),
-            },
-            Transform {
-                translation: position,
-                scale: Vec3::splat(radius),
-                ..default()
-            },
-        ));
+impl Mine {
+    fn random(rand: &mut rand::rngs::StdRng) -> Self {
+        Mine {
+            pos: Vec2::new(
+                (rand.gen::<f32>() - 0.5) * 1200.0,
+                (rand.gen::<f32>() - 0.5) * 600.0,
+            ),
+            size: 4.0 + rand.gen::<f32>() * 16.0,
+        }
     }
+}
 
-    // add bigger "star" body in the center
-    let star_radius = 1.;
-    commands
-        .spawn((
-            BodyBundle {
-                mesh: Mesh3d(meshes.add(Sphere::new(1.0).mesh().ico(5).unwrap())),
-                material: MeshMaterial3d(materials.add(StandardMaterial {
-                    base_color: ORANGE_RED.into(),
-                    emissive: LinearRgba::from(ORANGE_RED) * 2.,
-                    ..default()
-                })),
+#[derive(Event)]
+struct ExplodeMines {
+    pos: Vec2,
+    radius: f32,
+}
 
-                mass: Mass(500.0),
-                ..default()
-            },
-            Transform::from_scale(Vec3::splat(star_radius)),
-            Star,
-        ))
-        .with_child(PointLight {
-            color: Color::WHITE,
-            range: 100.0,
-            radius: star_radius,
-            ..default()
-        });
+#[derive(Event)]
+struct Explode;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
     commands.spawn((
-        Camera3d::default(),
-        Camera {
-            hdr: true,
-
+        Text::new(
+            "Click on a \"Mine\" to trigger it.\n\
+            When it explodes it will trigger all overlapping mines.",
+        ),
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(12.),
+            left: Val::Px(12.),
             ..default()
         },
-        Transform::from_xyz(0.0, 10.5, -30.0).looking_at(Vec3::ZERO, Vec3::Y),
-        Bloom::NATURAL,
     ));
-}
 
-fn interact_bodies(mut query: Query<(&Mass, &GlobalTransform, &mut Acceleration)>) {
-    let mut iter = query.iter_combinations_mut();
-    while let Some([(Mass(m1), transform1, mut acc1), (Mass(m2), transform2, mut acc2)]) =
-        iter.fetch_next()
-    {
-        let delta = transform2.translation() - transform1.translation();
-        let distance_sq: f32 = delta.length_squared();
+    let mut rng = rand::rngs::StdRng::seed_from_u64(19878367467713);
 
-        let f = GRAVITY_CONSTANT / distance_sq;
-        let force_unit_mass = delta * f;
-        acc1.0 += force_unit_mass * *m2;
-        acc2.0 -= force_unit_mass * *m1;
+    commands
+        .spawn(Mine::random(&mut rng))
+        // Observers can watch for events targeting a specific entity.
+        // This will create a new observer that runs whenever the Explode event
+        // is triggered for this spawned entity.
+        .observe(explode_mine);
+
+    // We want to spawn a bunch of mines. We could just call the code above for each of them.
+    // That would create a new observer instance for every Mine entity. Having duplicate observers
+    // generally isn't worth worrying about as the overhead is low. But if you want to be maximally efficient,
+    // you can reuse observers across entities.
+    //
+    // First, observers are actually just entities with the Observer component! The `observe()` functions
+    // you've seen so far in this example are just shorthand for manually spawning an observer.
+    let mut observer = Observer::new(explode_mine);
+
+    // As we spawn entities, we can make this observer watch each of them:
+    for _ in 0..1000 {
+        let entity = commands.spawn(Mine::random(&mut rng)).id();
+        observer.watch_entity(entity);
     }
+
+    // By spawning the Observer component, it becomes active!
+    commands.spawn(observer);
 }
 
-fn integrate(time: Res<Time>, mut query: Query<(&mut Acceleration, &mut Transform, &mut LastPos)>) {
-    let dt_sq = time.delta_secs() * time.delta_secs();
-    for (mut acceleration, mut transform, mut last_pos) in &mut query {
-        // verlet integration
-        // x(t+dt) = 2x(t) - x(t-dt) + a(t)dt^2 + O(dt^4)
-
-        let new_pos = transform.translation * 2.0 - last_pos.0 + acceleration.0 * dt_sq;
-        acceleration.0 = Vec3::ZERO;
-        last_pos.0 = transform.translation;
-        transform.translation = new_pos;
-    }
-}
-
-fn look_at_star(
-    mut camera: Single<&mut Transform, (With<Camera>, Without<Star>)>,
-    star: Single<&Transform, With<Star>>,
+fn on_add_mine(
+    trigger: Trigger<OnAdd, Mine>,
+    query: Query<&Mine>,
+    mut index: ResMut<SpatialIndex>,
 ) {
-    let new_rotation = camera
-        .looking_at(star.translation, Vec3::Y)
-        .rotation
-        .lerp(camera.rotation, 0.1);
-    camera.rotation = new_rotation;
+    let mine = query.get(trigger.entity()).unwrap();
+    let tile = (
+        (mine.pos.x / CELL_SIZE).floor() as i32,
+        (mine.pos.y / CELL_SIZE).floor() as i32,
+    );
+    index.map.entry(tile).or_default().insert(trigger.entity());
+}
+
+// Remove despawned mines from our index
+fn on_remove_mine(
+    trigger: Trigger<OnRemove, Mine>,
+    query: Query<&Mine>,
+    mut index: ResMut<SpatialIndex>,
+) {
+    let mine = query.get(trigger.entity()).unwrap();
+    let tile = (
+        (mine.pos.x / CELL_SIZE).floor() as i32,
+        (mine.pos.y / CELL_SIZE).floor() as i32,
+    );
+    index.map.entry(tile).and_modify(|set| {
+        set.remove(&trigger.entity());
+    });
+}
+
+fn explode_mine(trigger: Trigger<Explode>, query: Query<&Mine>, mut commands: Commands) {
+    // If a triggered event is targeting a specific entity you can access it with `.entity()`
+    let id = trigger.entity();
+    let Some(mut entity) = commands.get_entity(id) else {
+        return;
+    };
+    info!("Boom! {:?} exploded.", id.index());
+    entity.despawn();
+    let mine = query.get(id).unwrap();
+    // Trigger another explosion cascade.
+    commands.trigger(ExplodeMines {
+        pos: mine.pos,
+        radius: mine.size,
+    });
+}
+
+// Draw a circle for each mine using `Gizmos`
+fn draw_shapes(mut gizmos: Gizmos, mines: Query<&Mine>) {
+    for mine in &mines {
+        gizmos.circle_2d(
+            mine.pos,
+            mine.size,
+            Color::hsl((mine.size - 4.0) / 16.0 * 360.0, 1.0, 0.8),
+        );
+    }
+}
+
+// Trigger `ExplodeMines` at the position of a given click
+fn handle_click(
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    camera: Single<(&Camera, &GlobalTransform)>,
+    windows: Single<&Window>,
+    mut commands: Commands,
+) {
+    let (camera, camera_transform) = *camera;
+    if let Some(pos) = windows
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor).ok())
+        .map(|ray| ray.origin.truncate())
+    {
+        if mouse_button_input.just_pressed(MouseButton::Left) {
+            commands.trigger(ExplodeMines { pos, radius: 1.0 });
+        }
+    }
+}
+
+#[derive(Resource, Default)]
+struct SpatialIndex {
+    map: HashMap<(i32, i32), HashSet<Entity>>,
+}
+
+/// Cell size has to be bigger than any `TriggerMine::radius`
+const CELL_SIZE: f32 = 64.0;
+
+impl SpatialIndex {
+    // Lookup all entities within adjacent cells of our spatial index
+    fn get_nearby(&self, pos: Vec2) -> Vec<Entity> {
+        let tile = (
+            (pos.x / CELL_SIZE).floor() as i32,
+            (pos.y / CELL_SIZE).floor() as i32,
+        );
+        let mut nearby = Vec::new();
+        for x in -1..2 {
+            for y in -1..2 {
+                if let Some(mines) = self.map.get(&(tile.0 + x, tile.1 + y)) {
+                    nearby.extend(mines.iter());
+                }
+            }
+        }
+        nearby
+    }
 }
